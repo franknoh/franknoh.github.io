1ceaf3f7f3159678dbca8a0ed02485ed004cbe3c
franknoh <42113651+franknoh@users.noreply.github.com>
2020-10-20 12:49:34 +0900

    Create firebase-init.js

---
+++ script/firebase-init.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyC2uqV1aTqbeV8Ns10-9UeTBV5UWBlQ1AI",
+    authDomain: "kakaotalkbot-docs.firebaseapp.com",
+    databaseURL: "https://kakaotalkbot-docs.firebaseio.com",
+    projectId: "kakaotalkbot-docs",
+    storageBucket: "kakaotalkbot-docs.appspot.com",
+    messagingSenderId: "209657729121",
+    appId: "1:209657729121:web:94edd905e7135f9f315a20",
+    measurementId: "G-ET1Y9WY3Y8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
